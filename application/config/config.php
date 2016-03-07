d12c445a83d125ffcca902f37acd8ff5326239be
unknown <u1267493@CW501-19.AD.HUD.AC.UK>
2016-03-07 14:41:02 +0000

    updated config file and database file

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+$config['base_url']	= 'https://selene.hud.ac.uk/u1267493/task-manager/';
 
 /*
 |--------------------------------------------------------------------------
