210ec46c58a9f71c58f467b6f7364c197b431866
unknown <u1267493@CW301-22.AD.HUD.AC.UK>
2016-04-29 19:54:22 +0100

    on search page i set colour co-ordinate for each indvidual task to show task status

--- application/views/search.php
+++ application/views/search.php
@@ -37,7 +37,7 @@
 /*gains access to database*/
     require_once("config.inc.php");
 
-  $counter = 0;
+
 /*search box code (enter a search term or results won't display)*/
 if(isset($_POST['search']))
 {
@@ -58,23 +58,87 @@ $stmt->execute();
 
 /*the count starts from 0 and when the while loop goes around it collects how many tasks and Due dates have shown*/
 
-
+ $counter = 0;
 ob_start();
 echo "<br>";
 echo "<table id='table2' class='table2'>";
-echo "<tr><th colspan='5'>Your Tasks</th></tr>";
+echo "<tr><th colspan='5'>Task Status</th></tr>";
 echo "<tr class='table-header'><td>Task Name</td><td>Task Description</td><td>Due Date</td><td>Create Date/Time</td><td>Task Progress</td></tr>";
+$date_select = date("Y-m-d", strtotime('+3 days') );
+ 
 while ($tasks=$stmt->fetch(PDO::FETCH_OBJ))
 {
+	  
     //displays details of the tasks// /*this code loops around and collects the data from the search page*/
     echo "<tr>";
-	echo "<td><p><a href='" . base_url() . "index.php/addtask/updatetaskform/" . $tasks->id . "'> " . $tasks->task_name . "</a><br/></td>";
-	echo "<td>" . $tasks->task_description . "<br/></td>";
-	echo "<td> " . $tasks->due_date . "<br/></td>";
-	echo "<td>  " . $tasks->create_date . "<br/></td>";
-	echo "<td> " . $tasks->task_progress . "</p></td>";
-    
-      $counter++;
+		    
+		    if ($tasks->task_progress=="Ready"){
+			      if($tasks->due_date < $date_select){
+				echo "<td class='green'>";
+				echo $tasks->task_name;
+				echo "</td>";
+				echo "<td class='green'>";
+				echo $tasks->task_description;
+				echo "</td>";
+				echo "<td class='green'>";
+				echo $tasks->due_date;
+				echo "</td>";
+				echo "<td class='green'>";
+				echo $tasks->create_date;
+				echo "</td>";
+				echo "<td class='green'>";
+				echo $tasks->task_progress;
+				echo "</td>";
+				$counter++; 
+			      }
+		    }else if ($tasks->task_progress=="In Progress"){
+			      if($tasks->due_date < $date_select){
+				echo "<td class='yellow'>";
+				echo $tasks->task_name;
+				echo "</td>";
+				echo "<td class='yellow'>";
+				echo $tasks->task_description;
+				echo "</td>";
+				echo "<td class='yellow'>";
+				echo $tasks->due_date;
+				echo "</td>";
+				echo "<td class='yellow'>";
+				echo $tasks->create_date;
+				echo "</td>";
+				echo "<td class='yellow'>";
+				echo $tasks->task_progress;
+				echo "</td>";
+				$counter++; 
+			      }
+		    }else{
+			      if($tasks->due_date < $date_select){
+				echo "<td class='red'>";
+				echo $tasks->task_name;
+				echo "</td>";
+				echo "<td class='red'>";
+				echo $tasks->task_description;
+				echo "</td>";
+				echo "<td class='red'>";
+				echo $tasks->due_date;
+				echo "</td>";
+				echo "<td class='red'>";
+				echo $tasks->create_date;
+				echo "</td>";
+				echo "<td class='red'>";
+				echo $tasks->task_progress;
+				echo "</td>";
+				$counter++; 
+			      }
+			
+		    }
+		    
+		    
+		    
+		    //active record  gets the date from database and if the due date is less than the date 3 days from now it displays the task//
+		    
+		    echo "</tr>";
+         
+      
       
 }
 echo "</table>"; 
@@ -110,6 +174,7 @@ $conn=NULL;
 echo "<a href='" . base_url() . "index.php/login/home'>Back To Home Page</a>"; // link to go to the Homepage
 ?>
 </p>
-
+	   
+	  
 
 </div>
